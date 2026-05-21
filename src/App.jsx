f89034afe0ad4b3a5218bfc685485864812cf363
MaikeraW <wagner.maiconhenrique@gmail.com>
2026-05-21 15:43:17 -0300

    passando promisse por paramentro na pagina de detalhes dos produtos

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Layout from './assets/components/layout/Layout'
 import Home from './assets/pages/Home'
 import ProdutosSuspense from './assets/pages/ProdutosSuspense'
-import ProdutosDetalhes from './assets/pages/ProdutosDetalhes'
+import ProdutosDetalhesSuspense from './assets/pages/ProdutosDetalhesSuspense'
 
 
 import './App.css'
@@ -16,7 +16,7 @@ function App() {
         <Route path="/" element={<Layout/>}>
           <Route index element={<Home/>} />
           <Route path="produtos" element={<ProdutosSuspense/>} />
-          <Route path="produtos/:id" element={<ProdutosDetalhes/>} />
+          <Route path="produtos/:id" element={<ProdutosDetalhesSuspense/>} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/assets/pages/ProdutosDetalhesSuspense.jsx
@@ -0,0 +1,52 @@
+import { use, Suspense } from 'react'
+import { Link } from 'react-router-dom'
+import styles from './ProdutosDetalhes.module.css'
+import { useParams } from 'react-router-dom'
+
+    
+    
+
+function ProdutoBody({produtoDetalhadoPromise}){
+    
+    const produto = use(produtoDetalhadoPromise)
+
+    if(!produto) return <div>Produto não encontrado</div>
+
+    return(
+            
+            
+            <div className={styles.content}>
+                <div className={styles.image}>
+                    <img src={produto.image} alt={produto.title} />
+                </div>
+                <div className={styles.info}>
+                    <span className={styles.category}>{produto.category}</span>
+                    <h1 className={styles.title}>{produto.title}</h1>
+                    <p className={styles.description}>{produto.description}</p>
+                    <p className={styles.price}>{produto.price.toFixed(2).replace(".",",")}</p>
+                    <button className={styles.button}>Adicionar ao Carrinho</button>
+                </div>
+            </div>
+    )
+}
+
+function ProdutosDetalhesSuspense(){
+
+    const { id } = useParams()
+    const produtoDetalhadoPromise = fetch(`https://fakestoreapi.com/products/${id}`).then( res => res.json())
+
+    return(
+        <article className={styles.container}>
+            <Link to="/produtos" className={styles.backButton}> Voltar</Link>
+
+        
+        <Suspense fallback={<div><p>Loading Fallback</p></div>}>
+            <ProdutoBody produtoDetalhadoPromise={produtoDetalhadoPromise}/>
+        </Suspense>
+        
+        </article>
+        
+    )
+}
+
+export default ProdutosDetalhesSuspense
