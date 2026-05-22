50c39c568ba141d546c6d91544e88a240137a48e
MaikeraW <wagner.maiconhenrique@gmail.com>
2026-05-22 08:28:24 -0300

    criado contexto para o carrinho

---
+++ src/assets/contexts/CarrinhoContext.jsx
@@ -0,0 +1,27 @@
+import { createContext, useContext } from "react";
+
+
+const CarrinhoContext = createContext()
+
+export function CarrinhoProvider({children}){
+    const [itens, setItens] = useState([])
+
+    const adicionarCarrinho = (produto) => {
+        setItens( prev => {
+            const existe = prev.find( item => item.id === produto.id)
+
+            if(existe){
+                return prev.map( item => item.id === produto.id ? {...item, quantidade: item.quantidade + 1} : item)
+            }
+            return [...prev, {...produto, quantidade: 1}]
+        })
+        alert(`O produto com Id ${produto.id} foi adicionado ao carrinho`)
+    }
+
+    return <CarrinhoContext.Provider>
+                {children}
+           </CarrinhoContext.Provider>
+
+}
+
+export const useCarrinho = () => useContext(CarrinhoContext)
